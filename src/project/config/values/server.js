0e6ee144b766c2af5ac8eb3f96cfdd2ab9319618
jmarroyave-compsci <jmarroyave.compsci@gmail.com>
2021-09-12 12:20:37 -0400

    adding countries dashboard

--- src/project/config/values/server.js
+++ src/project/config/values/server.js
@@ -2,6 +2,7 @@ class Server {
 }
 Server.DATA = "http://localhost/data";
 Server.BASE_PATH = "/";
-Server.DATA_GRAPHQL = "http://localhost:8080/graphql";
+//Server.DATA_GRAPHQL = "http://localhost:8080/graphql";
+Server.DATA_GRAPHQL = "https://jmarroyave-data-server-01.herokuapp.com/graphql";
 
 export default Server;
