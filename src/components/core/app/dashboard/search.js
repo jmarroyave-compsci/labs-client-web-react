70387d29c28838aa0f7062b62404f7616784f314
jmarroyave-compsci <jmarroyave.compsci@gmail.com>
2021-11-01 15:16:05 -0400

    prerender

--- src/components/core/app/dashboard/search.js
+++ src/components/core/app/dashboard/search.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
+import theme from 'app/config/theme/main';
 
 const MIN_QUERY = 3
 
@@ -24,6 +26,7 @@ function onChange(qry, handler) {
 
 
 export default function SearchBox( props ) {
+  const [ loaded, setLoaded ] = React.useState(false);
   const { onSearchQuery, onSearchSuggestions } = props;
   const [open, setOpen] = React.useState(false);
   const [options, setOptions] = React.useState([]);
@@ -35,13 +38,6 @@ export default function SearchBox( props ) {
   React.useEffect(() => {
     let active = true;
 
-    //console.log(inputValue, 'voy')
-
-    /*
-    if (!loading) {
-      return undefined;
-    }
-    */
 
     (async () => {
       var dataSuggestions = await fetchSuggestions(inputValue, onSearchSuggestions);
@@ -51,55 +47,72 @@ export default function SearchBox( props ) {
       }
     })();
 
+    setLoaded(true);
+
     return () => {
       active = false;
     };
-  }, [loading, open, inputValue, onSearchSuggestions]);
+  }, [loading, open, inputValue, onSearchSuggestions. loaded]);
 
 
   return (
-    <Autocomplete
-      freeSolo
-      clearOnEscape
-      sx={{ flexGrow: 1 }}        
-      open={open}
-      onOpen={() => {
-        setOpen(true);
-      }}
-      onClose={() => {
-        setOpen(false);
-      }}
-      isOptionEqualToValue={(option, value) => option === value}
-      getOptionLabel={(option) => option}
-      options={options}
-      loading={loading}
-      value={value}
-      onChange={(event, newValue) => {
-        setOptions(newValue ? [newValue, ...options] : options);
-        setValue(newValue);
-        onChange(newValue, onSearchQuery)
-      }}
-      onInputChange={(event, newInputValue) => {
-        setInputValue(newInputValue);
-      }}
-      renderInput={(params) => (
-        <TextField
-          margin="dense"
-          {...params}
-          label="Search"
-          InputProps={{
-            ...params.InputProps,
-            type: 'searching',
-            endAdornment: (
-              <React.Fragment>
-                {loading ? <CircularProgress color="inherit" size={20} /> : null}
-                {params.InputProps.endAdornment}
-              </React.Fragment>
-            ),
+    <>
+      <div style={{display: (loaded) ? 'block' : 'none'}}>
+        <Autocomplete
+          freeSolo
+          clearOnEscape
+          sx={{ flexGrow: 1 }}
+          open={open}
+          onOpen={() => {
+            setOpen(true);
           }}
+          onClose={() => {
+            setOpen(false);
+          }}
+          isOptionEqualToValue={(option, value) => option === value}
+          getOptionLabel={(option) => option}
+          options={options}
+          loading={loading}
+          value={value}
+          onChange={(event, newValue) => {
+            setOptions(newValue ? [newValue, ...options] : options);
+            setValue(newValue);
+            onChange(newValue, onSearchQuery)
+          }}
+          onInputChange={(event, newInputValue) => {
+            setInputValue(newInputValue);
+          }}
+          renderInput={(params) => (
+            <TextField
+              margin="dense"
+              {...params}
+              label="Search"
+              InputProps={{
+                ...params.InputProps,
+                type: 'searching',
+                endAdornment: (
+                  <React.Fragment>
+                    {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                    {params.InputProps.endAdornment}
+                  </React.Fragment>
+                ),
+              }}
+            />
+          )}
         />
-      )}
-    />
+      </div>
+      <div style={{
+          flexGrow: 1, 
+          display: (!loaded) ? 'block' : 'none',
+          borderRadius: '4px',
+          color: 'rgba(0,0,0,0.30)',
+          border: '1px solid',
+          borderColor: 'rgba(0,0,0,0.25)',
+          padding: '1rem',
+        }}>
+        Search
+      </div>
+    </>
   );
 }
 
