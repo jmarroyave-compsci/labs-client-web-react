3d2e2b42df44f1ff8266256e6c0c9d83a21ccb76
jmarroyave-compsci <jmarroyave.compsci@gmail.com>
2021-11-03 10:14:07 -0400

    fixes

--- docs/app/_next/static/50Hv910VEq0j0KY0BdqVP/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fabout\u002Fproject","\u002F","\u002Fabout\u002Fproject\u002Flog\u002F[[...index]]","\u002Fabout\u002Fproject\u002Flog\u002Fposts\u002F[index]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
