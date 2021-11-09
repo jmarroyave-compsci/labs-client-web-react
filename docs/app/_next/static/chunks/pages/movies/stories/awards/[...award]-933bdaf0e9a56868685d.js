5172dee7fdcdc350a33695aafba5c3e7a1aa7ddf
jmarroyave-compsci <jmarroyave.compsci@gmail.com>
2021-11-09 12:34:57 -0500

    docs

---
+++ docs/app/_next/static/chunks/pages/movies/stories/awards/[...award]-933bdaf0e9a56868685d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4627],{84553:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/movies/stories/awards/[...award]",function(){return u(56787)}])}},function(n){n.O(0,[6499,5685,5218,1216,5619,6787,9774,2888,179],(function(){return _=84553,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
