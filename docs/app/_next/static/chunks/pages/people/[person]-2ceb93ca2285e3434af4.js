08a46566dd83a225f982429aff8dc251df943fd7
jmarroyave-compsci <jmarroyave.compsci@gmail.com>
2021-11-10 16:09:14 -0500

    docs

--- docs/app/_next/static/chunks/pages/people/[person]-2ceb93ca2285e3434af4.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4054],{54317:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/people/[person]",function(){return u(8126)}])}},function(n){n.O(0,[6499,5685,5218,1216,5619,5630,8126,9774,2888,179],(function(){return _=54317,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
