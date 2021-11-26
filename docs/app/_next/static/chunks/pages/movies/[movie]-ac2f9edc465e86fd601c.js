5f54ca0f1911d12abab77f0f48a539e997b0914d
jmarroyave-compsci <jmarroyave.compsci@gmail.com>
2021-11-25 23:12:44 -0500

    updating demo and docs

---
+++ docs/app/_next/static/chunks/pages/movies/[movie]-ac2f9edc465e86fd601c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4132],{91731:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/movies/[movie]",function(){return u(35743)}])}},function(n){n.O(0,[6499,5685,3128,5218,4471,1216,4197,7338,7792,5863,5743,9774,2888,179],(function(){return _=91731,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
--- docs/app/_next/static/chunks/pages/people/[person]-82aa276ef04ec7626a1b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4054],{54317:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/people/[person]",function(){return u(14023)}])}},function(n){n.O(0,[6499,5685,3128,5218,4471,1216,4197,4999,2282,5863,4023,9774,2888,179],(function(){return _=54317,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
