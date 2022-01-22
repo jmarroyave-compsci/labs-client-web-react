7a81de837510a2e2289de9cbb8735850c1f9c5ee
jmarroyave-compsci <jmarroyave.compsci@gmail.com>
2022-01-21 20:14:51 -0500

    ui adjustments

--- src/components/com/pages/navigator/com/history/index.js
+++ src/components/com/pages/navigator/com/history/index.js
@@ -8,6 +8,7 @@ import YearParameter from './year-parameter'
 import TagParameter from './tag-parameter'
 import HistoryData from './history-data'
 import Help from "../help"
+import { useSwipeable } from 'react-swipeable';
 
 function History(props){
     const [ history, setHistory ] = useState( {} )
@@ -23,6 +24,23 @@ function History(props){
       setHistory(d);
     }, [ currentDecade ])
 
+    const slide = (direction) => {
+      if(direction == "NEXT" && year < 9){
+        setYear(year + 1)
+      }
+
+      if(direction == "PREV" && decade > 0){
+        setYear(year - 1)
+      }
+    }
+
+    const handlers = useSwipeable({
+      onSwipedLeft: () => slide("NEXT"),
+      onSwipedRight: () => slide("PREV"),
+      preventDefaultTouchmoveEvent: true,
+      trackMouse: true
+    });
+
     return (
       <Frame>
         <Stack style={{ display: 'flex', overflow: 'hidden', height: "100%"}}>
@@ -33,9 +51,11 @@ function History(props){
           <Frame border style={{display: 'flex', flexDirection: 'column', height: '100%'}}>
             <YearParameter disabled={tagsSelected != null} data={history} current={year} onClick={(year) => setYear(year) } />
             <TagParameter data={history} onClick={(w) => setTagsSelected( w ) } />
-            <Scrollbars height={'100%'} style={{ height: '100%'}}>
-              <HistoryData data={history} tagsSelected={tagsSelected} year={year} onClick={(w) => context.dispatch( { type: "SELECT_TOPIC", payload: w } ) }/>
-            </Scrollbars>
+            <div  {...handlers}>
+              <Scrollbars height={'100%'} style={{ height: '100%'}}>
+                <HistoryData data={history} tagsSelected={tagsSelected} year={year} onClick={(w) => context.dispatch( { type: "SELECT_TOPIC", payload: w } ) }/>
+              </Scrollbars>
+            </div>
           </Frame>
         </Stack>
       </Frame>
